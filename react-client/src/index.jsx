12b4b49b22fd3fd9dfeea51a932d3072dd841ac1
samzoll22 <sam.zoll22@gmail.com>
2017-05-02 20:36:56 -0700

    update port and react setup

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -6,29 +6,29 @@ import List from './components/List.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       items: []
     }
   }
 
-  componentDidMount() {
-    $.ajax({
-      url: '/items', 
-      success: (data) => {
-        this.setState({
-          items: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
-    });
-  }
+  // componentDidMount() {
+  //   $.ajax({
+  //     url: '/items',
+  //     success: (data) => {
+  //       this.setState({
+  //         items: data
+  //       })
+  //     },
+  //     error: (err) => {
+  //       console.log('err', err);
+  //     }
+  //   });
+  // }
 
   render () {
-    return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
+    return (
+    <div>
+      <h1>Sam Zoll</h1>
     </div>)
   }
 }
--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.get('/items', function (req, res) {
   });
 });
 
-app.listen(3000, function() {
-  console.log('listening on port 3000!');
+app.listen(8000, function() {
+  console.log('listening on port 8000!');
 });
 
