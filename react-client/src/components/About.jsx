c02556c8a02b6f0b50407df00941a4017afbf118
samzoll22 <sam.zoll22@gmail.com>
2017-05-15 10:17:17 -0700

    adjust about section

--- react-client/src/components/About.jsx
+++ react-client/src/components/About.jsx
@@ -6,15 +6,15 @@ let Link       = Scroll.Link;
 
 const About = () => (
   <div >
-    <Well className="white center">
-      <Col md={12} >
-        <Image src="https://s3-us-west-1.amazonaws.com/zollstorage/portfolio/sam-i-am-text.svg" className="samTextSpacer" responsive width='300' />
+    <Well className="white center contactPadd">
+      <Row>
+        <Col md={10} mdOffset={1}>
+          <Image src="https://s3-us-west-1.amazonaws.com/zollstorage/portfolio/sam-i-am-text.svg" className="samTextSpacer" responsive width='300' />
 
-            <h3 className="samFont"> I don't like green eggs and ham, but I do love designing beautifully assembled products that focus on creating meaningful user experiences. In particular, I'm passionate about crafting experiences from start to finish, from sketches to mockups, to a coded beautiful masterpiece. In my previous position at Purple Gator, I led a remote design team to create a trivia game user experience which has been ranked in the top casino apps in the US Apple App Store. Recently, I've built several applications including a food truck finding and ordering app, a real-time multiplayer text based game, and a ticket price comparison app.
-            </h3>
-      </Col>
-    </Well>
-    <Well className="contactPadd">
+              <h3 className="samFont center"> I don't like green eggs and ham, but I do love designing beautifully assembled products that focus on creating meaningful user experiences. In particular, I'm passionate about crafting experiences from start to finish, from sketches to mockups, to a coded beautiful masterpiece. In my previous position at Purple Gator, I led a remote design team to create a trivia game user experience which has been ranked in the top casino apps in the US Apple App Store. Recently, I've built several applications including a food truck finding and ordering app, a real-time multiplayer text based game, and a ticket price comparison app.
+              </h3>
+        </Col>
+      </Row>
       <Row className="center miniSpacer">
         <h2> Toolbox Coming Soon </h2>
       </Row>
@@ -27,6 +27,7 @@ const About = () => (
         </Col>
       </Row>
     </Well>
+
   </div>
 )
 
