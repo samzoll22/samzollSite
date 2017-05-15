6dc57f244b7be1756fd8001a816cd6cd594c3517
samzoll22 <sam.zoll22@gmail.com>
2017-05-15 10:11:53 -0700

    remove silly about picture

--- react-client/src/components/About.jsx
+++ react-client/src/components/About.jsx
@@ -6,8 +6,8 @@ let Link       = Scroll.Link;
 
 const About = () => (
   <div >
-    <Well className="aboutParent white samgif center">
-      <Col md={6} >
+    <Well className="white center">
+      <Col md={12} >
         <Image src="https://s3-us-west-1.amazonaws.com/zollstorage/portfolio/sam-i-am-text.svg" className="samTextSpacer" responsive width='300' />
 
             <h3 className="samFont"> I don't like green eggs and ham, but I do love designing beautifully assembled products that focus on creating meaningful user experiences. In particular, I'm passionate about crafting experiences from start to finish, from sketches to mockups, to a coded beautiful masterpiece. In my previous position at Purple Gator, I led a remote design team to create a trivia game user experience which has been ranked in the top casino apps in the US Apple App Store. Recently, I've built several applications including a food truck finding and ordering app, a real-time multiplayer text based game, and a ticket price comparison app.
@@ -20,10 +20,10 @@ const About = () => (
       </Row>
       <Row >
         <Col sm={6} className="center miniSpacer">
-          <Image className="aboutImages" src="https://s3-us-west-1.amazonaws.com/zollstorage/portfolio/design-tools.png"  />
+          <Image className="aboutImagesDesign" src="https://s3-us-west-1.amazonaws.com/zollstorage/portfolio/design-tools.png"  />
         </Col>
         <Col sm={6} className="center miniSpacer">
-          <Image className="aboutImages" src="https://s3-us-west-1.amazonaws.com/zollstorage/portfolio/developer-tools.png"  />
+          <Image className="aboutImagesDevelop" src="https://s3-us-west-1.amazonaws.com/zollstorage/portfolio/developer-tools.png"  />
         </Col>
       </Row>
     </Well>
